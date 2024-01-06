2c4c32d98eaa7a68afe6f5ed9cd7927ec01cb305
eminakgun <eminakgn@outlook.com>
2024-01-06 15:45:50 +0300

    Add iterator for File and Directory

--- include/directory.hpp
+++ include/directory.hpp
@@ -14,6 +14,37 @@ public:
     Directory(const std::string& name);
     //friend std::ostream& operator<<(std::ostream& os, const Directory& f);
 
+    // Iterator Interface
+    class Iterator {
+        private:
+            const std::vector<Directory*>& directories;
+            typename std::vector<Directory*>::const_iterator current;
+
+        public:
+            Iterator(const std::vector<Directory*>& directories) 
+                : directories(directories), current(directories.begin()) {}
+
+            bool operator!=(const Iterator& other) const {
+                return current != other.current;
+            }
+
+            const Directory* operator*() const {
+                return *current;
+            }
+
+            Iterator& operator++() {
+                ++current;
+                return *this;
+            }
+        };
+
+    Iterator begin() const {
+        return Iterator(childs);
+    }
+
+    Iterator end() const {
+        //
+    }
 };
 
 #endif
--- include/file.hpp
+++ include/file.hpp
@@ -27,6 +27,39 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const File& f);
 
     virtual size_t size() {return content.size();}
+
+    // Iterator Interface for the file content
+    class Iterator {
+    private:
+        const std::string& content;
+        size_t current;
+
+    public:
+        Iterator(const std::string& content) : content(content), current(0) {}
+        Iterator(const std::string& content, size_t current) : content(content), current(current) {}
+
+        bool operator!=(const Iterator& other) const {
+            return current != other.current;
+        }
+
+        char operator*() const {
+            return content[current];
+        }
+
+        Iterator& operator++() {
+            ++current;
+            return *this;
+        }
+    };
+
+    // Functions to get iterators for content
+    Iterator begin() const {
+        return Iterator(content);
+    }
+
+    Iterator end() const {
+        return Iterator(content, content.size() - 1);
+    }
 };
 
 #endif
