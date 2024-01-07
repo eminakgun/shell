0090c64483d66b5c397a77977f0377893be9c913
eminakgun <eminakgn@outlook.com>
2024-01-07 11:55:09 +0300

    Update iteratros in Directory

--- include/directory.hpp
+++ include/directory.hpp
@@ -10,19 +10,29 @@ class Directory : public File
 private:
     std::vector<File*> files;
     std::vector<Directory*> childs;
+    std::string full_path;
+
 public:
-    Directory(const std::string& name);
-    //friend std::ostream& operator<<(std::ostream& os, const Directory& f);
+    Directory(const std::string& name) : File(name), full_path("") {symbol = "D";}
+    Directory(const std::string& name, const std::string& full_path) 
+        : File(name, "D"), full_path(full_path) {};
+    
+    void add_file(File* file);
+    void add_subdir(Directory* dir);
+    std::string& get_full_path() {return full_path;}
 
     // Iterator Interface
     class Iterator {
         private:
-            const std::vector<Directory*>& directories;
+            //const std::vector<Directory*>& directories;
             typename std::vector<Directory*>::const_iterator current;
 
         public:
-            Iterator(const std::vector<Directory*>& directories) 
-                : directories(directories), current(directories.begin()) {}
+            // Iterator(const std::vector<Directory*>& directories) 
+            //     : directories(directories), current(directories.begin()) {}
+            
+            Iterator(std::vector<Directory*>::const_iterator current) 
+                : current(current) {}
 
             bool operator!=(const Iterator& other) const {
                 return current != other.current;
@@ -38,12 +48,44 @@ public:
             }
         };
 
+    class FileIterator {
+        private:
+            //const std::vector<File*>& files;
+            std::vector<File*>::const_iterator current;
+
+        public:
+            FileIterator(std::vector<File*>::const_iterator current) 
+                : current(current) {}
+            
+            bool operator!=(const FileIterator& other) const {
+                return current != other.current;
+            }
+
+            const File* operator*() const {
+                return *current;
+            }
+
+            FileIterator& operator++() {
+                ++current;
+                return *this;
+            }
+        };
+
     Iterator begin() const {
-        return Iterator(childs);
+        return Iterator(childs.begin());
     }
 
     Iterator end() const {
         //
+        return Iterator(childs.end());
+    }
+
+    FileIterator fbegin() const {
+        return FileIterator(files.begin());
+    }
+
+    FileIterator fend() const {
+        return FileIterator(files.end());
     }
 };
 
